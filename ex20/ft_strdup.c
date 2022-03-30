8da576740cfd50bc1757591ec90f8d1d703259a6
asoler <asoler@student.42sp.org.br>
2022-03-30 16:04:02 +0200

    most initilize each position of the array

--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 16:30:10 by asoler            #+#    #+#             */
-/*   Updated: 2022/03/29 16:41:38 by asoler           ###   ########.fr       */
+/*   Updated: 2022/03/30 16:01:57 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,32 @@ char	*ft_strdup(char *src)
 {
 	char	*dup;
 	int		size;
+	int		i;
 
+	i = 0; 
 	size = ft_strlen(src);
-	dup = (char *)malloc(size);
-	dup = src;
+	dup = (char *)malloc(sizeof(char) * size);
+	while (i < size)
+	{
+		dup[i] = src[i];
+		i++;
+	}
+	dup[i] = src[i];
 	return (dup);
+}
+
+#include <string.h>
+#include <stdio.h>
+int	main()
+{
+	char str[] = "hello 42";
+	char *dup = strdup(str);
+	char *ftdup = ft_strdup(str);
+
+	printf("Original Funtion: %s\n\n", dup);
+	printf("Original String: %s\n\n", str);
+	printf("FT Funtion: %s\n\n", ftdup);
+	printf("Original Funtion adress: %p\n\n", dup);
+	printf("Original String adress: %p\n\n", str);
+	printf("FT Funtion adress: %p\n\n", ftdup);
 }
